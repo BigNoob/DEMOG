9afb3049ed94496fd63ea7bec23fb302d27e23cc
BigNoob <debove.stephane@gmail.com>
2014-08-24 16:06:31 +0200

    changement des messages pop up et des instructions en cours de jeu

--- public/js/game/main_rabbits_client.js
+++ public/js/game/main_rabbits_client.js
@@ -551,13 +551,13 @@ function DrawGivenAmmount(data, role)
   var recieved = 3000 - parseInt(data);
   if(role == "RECIEVER")
   { 
-    alert("The other player shared the loot and gave you "+recieved+" points. Click to continue to the next game and wait for the next game to start." );
+    alert("The other player shared the loot and gave you "+recieved+" out of 3000 points. \n Click to continue and wait for the next game to start." );
     socket.emit("message",'ENDED'); 
   }
   else if(role == "SHARER")
   {
     
-    alert("You have given "+recieved+" points out of 3000 to the other player.\n Your points for this game are thus "+data+".\n Click to continue to the next game and wait for the next game to start." );
+    alert("You have given "+recieved+" out of 3000 points to the other player.\n Your points for this game are thus "+data+".\n Click to continue and wait for the next game to start." );
     socket.emit("message",'ENDED');
   }
 }
